cdbe3f5fab1149b2068c3a41ab91ae45f4b592dc
Xavier Dunkley <xavierxv18@gmail.com>
2022-10-08 18:38:47 -0400

    added logo

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header(){
         <h1>
             Welcome to Grumble
             <span className="logo" role="img">
-
+                <img src="https://static.vecteezy.com/system/resources/previews/003/212/212/non_2x/cute-cartoon-panda-wearing-headphone-vector.jpg" height={140} width={140}/>
             </span>
         </h1>
     </header>)
--- src/MusicRatings.js
+++ src/MusicRatings.js
@@ -14,9 +14,9 @@ function HandleRatings(){
 
     return(
         <div className="ratings">
-                <button onClick={handleLikes}><img src="https://www.macmillandictionary.com/external/slideshow/full/emoji_thumbs-up_full.jpg" height={30} width={30}/></button>
+                <button onClick={handleLikes}><img src="https://www.macmillandictionary.com/external/slideshow/full/emoji_thumbs-up_full.jpg" height={25} width={25}/></button>
                 <p>{likes}</p>
-                <button onClick={handleDislikes}><img src="https://www.macmillandictionary.com/external/slideshow/full/emoji_thumbs-down_full.jpg" height={30} width={30}/></button>
+                <button onClick={handleDislikes}><img src="https://www.macmillandictionary.com/external/slideshow/full/emoji_thumbs-down_full.jpg" height={25} width={25}/></button>
                 <p>{dislikes}</p>
         </div>
     )
