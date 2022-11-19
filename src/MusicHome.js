9dc31cc31720afd00aec364e178c2e76d18aebda
Xavier Dunkley <xavierxv18@gmail.com>
2022-11-18 22:10:44 -0500

    Completed Grumble Intro

--- src/MusicHome.js
+++ src/MusicHome.js
@@ -4,9 +4,7 @@ import React from "react";
 function Intro(){
 
     return(
-        <h3>
-            Hi
-        </h3>
+        <h3 className="intro">Hi! Welcome to Grumble. At Grumble, you can add information about different albums, like and dislike or delete them. Try it out!</h3>
     )
 
 
